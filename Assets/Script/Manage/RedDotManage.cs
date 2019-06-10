2370e05d3cd10e83bca3120d449b960f7d395771
XiongTwo <419318386@qq.com>
2019-06-10 17:54:58 +0800

    红点
    红点

---
+++ Assets/Script/Manage/RedDotManage.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class RedDotVo
+{
+    public bool state = false;
+    public int num = 0;
+    public List<RedDotEnum> byCiteList = new List<RedDotEnum>();
+}
+
+public class RedDotManage
+{
+    private static Dictionary<RedDotEnum, Action<RedDotVo>> redDotFunDic = new Dictionary<RedDotEnum, Action<RedDotVo>>();
+    private static Dictionary<RedDotEnum, RedDotVo> redDotVoDic = new Dictionary<RedDotEnum, RedDotVo>();
+    private static Dictionary<RedDotEnum, RedDotEnum[]> multiModeKeyList = new Dictionary<RedDotEnum, RedDotEnum[]>();
+
+    public static void Register(RedDotEnum key, Action<RedDotVo> fun , params RedDotEnum[] array)
+    {
+        redDotFunDic.Add(key, fun);
+        RedDotVo redDotVo = new RedDotVo();
+        redDotVoDic.Add(key, redDotVo);
+        if (array.Length>0)
+        {
+            multiModeKeyList.Add(key, array);
+        }
+        for (int i = 0; i < array.Length; i++)
+        {
+            redDotVoDic[array[i]].byCiteList.Add(key);
+        }
+    }
+
+    public static void Execute(RedDotEnum key)
+    {
+        if (multiModeKeyList.ContainsKey(key))
+        {
+            RedDotEnum[] array = multiModeKeyList[key];
+            for (int i = 0; i < array.Length; i++)
+            {
+                Execute(array[i]);
+            }
+        }
+        else
+        {
+            Action<RedDotVo> fun = redDotFunDic[key];
+            RedDotVo redDotVo = redDotVoDic[key];
+            fun.Invoke(redDotVo);
+            SetMultiModeRedDotVo(redDotVo);
+        }
+    }
+
+    //设置多个模式的RedDotVo
+    private static void SetMultiModeRedDotVo(RedDotVo redDotVo)
+    {
+        for (int i = 0; i < redDotVo.byCiteList.Count; i++)
+        {
+            RedDotEnum multiModeKey = redDotVo.byCiteList[i];
+            Action<RedDotVo> multiModeFun = redDotFunDic[multiModeKey];
+            RedDotVo multiModeRedDotVo = redDotVoDic[multiModeKey];
+
+            RedDotEnum[] array = multiModeKeyList[multiModeKey];
+            bool state = false;
+            int num = 0;
+            for (int j = 0; j < array.Length; j++)
+            {
+                RedDotVo vo = redDotVoDic[array[j]];
+                num += vo.num;
+                if (j == 0)
+                    state = vo.state;
+                else
+                    state = state && vo.state;
+            }
+            multiModeRedDotVo.num = num;
+            multiModeRedDotVo.state = state;
+            multiModeFun.Invoke(multiModeRedDotVo);
+        }
+    }
+
+    public static RedDotVo GetRedDotVo(RedDotEnum key)
+    {
+        return redDotVoDic[key];
+    }
+}
---
+++ Assets/Script/TestScript/TestRedDot.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestRedDot : MonoBehaviour {
+
+	void Start () {
+        RedDotManage.Register(RedDotEnum.FirstRedDot, (redDotVo) =>
+        {
+            redDotVo.state = true;
+            redDotVo.num = 1;
+        });
+
+        RedDotManage.Register(RedDotEnum.SecondRedDot, (redDotVo) =>
+        {
+            redDotVo.state = true;
+            redDotVo.num = 2;
+        });
+
+        RedDotManage.Register(RedDotEnum.ThirdRedDot, (redDotVo) =>
+        {
+            redDotVo.state = true;
+            redDotVo.num = 3;
+        });
+
+        RedDotManage.Register(RedDotEnum.MultiRedDot, (redDotVo) =>
+        {
+            Debug.LogError("触发的多条件判断红点刷新:" + RedDotEnum.MultiRedDot.ToString());
+            Debug.LogError(redDotVo.state);
+            Debug.LogError(redDotVo.num);
+            Debug.LogError("************************");
+        }, RedDotEnum.FirstRedDot, RedDotEnum.SecondRedDot, RedDotEnum.ThirdRedDot);
+
+        RefreshRedDot(RedDotEnum.FirstRedDot);
+        RefreshRedDot(RedDotEnum.SecondRedDot);
+        RefreshRedDot(RedDotEnum.ThirdRedDot);
+
+        RefreshRedDot(RedDotEnum.MultiRedDot);
+    }
+	
+    void RefreshRedDot(RedDotEnum e)
+    {
+        RedDotManage.Execute(e);
+        RedDotVo vo = RedDotManage.GetRedDotVo(e);
+        Debug.LogError("红点刷新:" + e.ToString());
+        Debug.LogError(vo.state);
+        Debug.LogError("************************");
+    }
+
+    void Update () {
+		
+	}
+}
